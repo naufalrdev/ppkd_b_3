72daa2f0ba2d6843aec6ba8796eb2dbf4331b399
naufalrdp0400 <naufalrifkidwiputra3081@gmail.com>
2025-08-05 09:59:17 +0700

    Final Task_3_4

--- lib/tugas_3-flutter/tugas_3.dart
+++ lib/tugas_3-flutter/tugas_3.dart
@@ -9,185 +9,188 @@ class Tugas3Flutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: EdgeInsets.all(60),
-                child: Text(
-                  "Welcome",
-                  style: TextStyle(fontFamily: "Lobster", fontSize: 40),
-                ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(60),
+              child: Text(
+                "Welcome",
+                style: TextStyle(fontFamily: "Lobster", fontSize: 40),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: AppColor.teal,
+                    ),
+                    child: Center(child: Text("Sign Up")),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white,
+                    ),
+                    child: Center(child: Text("Sign in")),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Name"),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Please enter your full name",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text("Email"),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Please enter your email address",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text("Phone Number"),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Please enter your phone number",
+                  prefixIcon: Icon(Icons.phone),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text("Describe"),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Please briefly describe yourself",
+                  prefixIcon: Icon(Icons.description),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(20),
+                  child: Icon(FontAwesomeIcons.scroll),
+                ),
+              ),
+            ],
+          ),
+
+          GridView.count(
+            shrinkWrap: true,
+            padding: EdgeInsets.all(8.0),
+            crossAxisCount: 3,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            children: [
+              Stack(
+                alignment: Alignment.center,
                 children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: AppColor.teal,
-                      ),
-                      child: Center(child: Text("Sign Up")),
-                    ),
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.pinkPastel),
                   ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.white,
-                      ),
-                      child: Center(child: Text("Sign in")),
-                    ),
-                  ),
+                  Text("Warna 1"),
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Name"),
-                SizedBox(height: 15),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Please enter your full name",
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.kuningPastel),
                   ),
-                ),
-                SizedBox(height: 15),
-                Text("Email"),
-                SizedBox(height: 15),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Please enter your email address",
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
+                  Text("Warna 2"),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.biruMuda),
                   ),
-                ),
-                SizedBox(height: 15),
-                Text("Phone Number"),
-                SizedBox(height: 15),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Please enter your phone number",
-                    prefixIcon: Icon(Icons.phone),
-                    border: OutlineInputBorder(),
+                  Text("Warna 3"),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.hijauPastel),
                   ),
-                ),
-                SizedBox(height: 15),
-                Text("Describe"),
-                SizedBox(height: 15),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Please briefly describe yourself",
-                    prefixIcon: Icon(Icons.description),
-                    border: OutlineInputBorder(),
+                  Text("Warna 4"),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.peach1),
                   ),
-                ),
-              ],
-            ),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Padding(
-                    padding: EdgeInsetsGeometry.all(20),
-                    child: Icon(FontAwesomeIcons.scroll),
+                  Text("Warna 5"),
+                ],
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 250,
+                    decoration: BoxDecoration(color: AppColor.merahMudaLembut),
                   ),
-                ),
-              ],
-            ),
-
-            GridView.count(
-              shrinkWrap: true,
-              padding: EdgeInsets.all(8.0),
-              crossAxisCount: 3,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(color: AppColor.pinkPastel),
-                    ),
-                    Text("Warna 1"),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(color: AppColor.kuningPastel),
-                    ),
-                    Text("Warna 2"),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(color: AppColor.biruMuda),
-                    ),
-                    Text("Warna 3"),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(color: AppColor.hijauPastel),
-                    ),
-                    Text("Warna 4"),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(color: AppColor.peach1),
-                    ),
-                    Text("Warna 5"),
-                  ],
-                ),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 250,
-                      decoration: BoxDecoration(
-                        color: AppColor.merahMudaLembut,
-                      ),
-                    ),
-                    Text("Warna 6 "),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+                  Text("Warna 6 "),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/tugas_4-flutter/tugas_4.dart
+++ lib/tugas_4-flutter/tugas_4.dart
@@ -7,24 +7,25 @@ class Tugas4Flutter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, dynamic>> listProduct = [
-      {"nama": "Kursi Ergonomis", "harga": "Rp 8.500.000", "icon": Icons.chair},
-      {"nama": "Sepeda", "harga": "Rp 5.000.000", "icon": Icons.pedal_bike},
-      {
-        "nama": "Monitor Gaming",
-        "harga": "Rp 1.500.000",
-        "icon": Icons.monitor,
-      },
-      {"nama": "Mouse Wireless", "harga": "Rp 500.000", "icon": Icons.mouse},
-      {
-        "nama": "Keyboard Gaming",
-        "harga": "Rp 560.000",
-        "icon": Icons.keyboard,
-      },
-    ];
+    // final List<Map<String, dynamic>> listProduct = [
+    //   {"nama": "Kursi Ergonomis", "harga": "Rp 8.500.000", "icon": Icons.chair},
+    //   {"nama": "Sepeda", "harga": "Rp 5.000.000", "icon": Icons.pedal_bike},
+    //   {
+    //     "nama": "Monitor Gaming",
+    //     "harga": "Rp 1.500.000",
+    //     "icon": Icons.monitor,
+    //   },
+    //   {"nama": "Mouse Wireless", "harga": "Rp 500.000", "icon": Icons.mouse},
+    //   {
+    //     "nama": "Keyboard Gaming",
+    //     "harga": "Rp 560.000",
+    //     "icon": Icons.keyboard,
+    //   },
+    // ];
 
     return Scaffold(
       body: ListView(
+        padding: EdgeInsets.all(16),
         children: [
           Column(
             children: [
@@ -49,7 +50,7 @@ class Tugas4Flutter extends StatelessWidget {
                     padding: EdgeInsets.all(16),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8),
-                      color: AppColor.teal,
+                      color: Colors.greenAccent,
                     ),
                     child: Center(child: Text("Sign Up")),
                   ),
@@ -91,7 +92,9 @@ class Tugas4Flutter extends StatelessWidget {
                 decoration: InputDecoration(
                   hintText: "Please enter your email address",
                   prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
               SizedBox(height: 15),
@@ -101,7 +104,9 @@ class Tugas4Flutter extends StatelessWidget {
                 decoration: InputDecoration(
                   hintText: "Please enter your phone number",
                   prefixIcon: Icon(Icons.phone),
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
               SizedBox(height: 15),
@@ -111,7 +116,9 @@ class Tugas4Flutter extends StatelessWidget {
                 decoration: InputDecoration(
                   hintText: "Please briefly describe yourself",
                   prefixIcon: Icon(Icons.description),
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
             ],
@@ -122,22 +129,147 @@ class Tugas4Flutter extends StatelessWidget {
             children: [
               Center(
                 child: Padding(
-                  padding: EdgeInsetsGeometry.all(20),
-                  child: Icon(FontAwesomeIcons.scroll),
+                  padding: EdgeInsetsGeometry.all(40),
+                  child: Icon(FontAwesomeIcons.arrowDown, size: 50),
                 ),
               ),
             ],
           ),
-          ListView(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            children: listProduct.map((produk) {
-              return ListTile(
-                leading: Icon(produk['icon']),
-                title: Text(produk['nama']),
-                subtitle: Text(produk['harga']),
-              );
-            }).toList(),
+          SizedBox(height: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(16),
+                  child: Text(
+                    "Product List",
+                    style: TextStyle(fontFamily: "Lobster", fontSize: 40),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // ListView(
+          //   shrinkWrap: true,
+          //   physics: NeverScrollableScrollPhysics(),
+          //   children: listProduct.map((produk) {
+          //     return ListTile(
+          //       leading: Icon(produk['icon']),
+          //       title: Text(produk['nama']),
+          //       subtitle: Text(produk['harga']),
+          //     );
+          //   }).toList(),
+          // ),
+
+          // ListTile(
+          //   leading: Icon(Icons.chair),
+          //   title: Text("Ergonom Chair"),
+          //   subtitle: Text("Rp8.500.000"),
+          //   trailing: Icon(Icons.shopping_cart),
+          // ),
+
+          // ListTile(
+          //   leading: Icon(Icons.pedal_bike),
+          //   title: Text("Road Bike"),
+          //   subtitle: Text("Rp 5.000.000"),
+          //   trailing: Icon(Icons.shopping_cart),
+          // ),
+
+          // ListTile(
+          //   leading: Icon(Icons.monitor),
+          //   title: Text("Monitor Gaming"),
+          //   subtitle: Text("Rp 1.500.000"),
+          //   trailing: Icon(Icons.shopping_cart),
+          // ),
+
+          // ListTile(
+          //   leading: Icon(Icons.mouse),
+          //   title: Text("Wireless Mouse"),
+          //   subtitle: Text("Rp 500.000"),
+          //   trailing: Icon(Icons.shopping_cart),
+          // ),
+
+          // ListTile(
+          //   leading: Icon(Icons.keyboard),
+          //   title: Text("Mechanical Keyboard Wireless"),
+          //   subtitle: Text("Rp 560.000"),
+          //   trailing: Icon(Icons.shopping_cart),
+          // ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Card(
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.chair),
+                title: Text('Ergonom Chair'),
+                subtitle: Text("Rp8.500.000"),
+                trailing: Icon(FontAwesomeIcons.bagShopping),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Card(
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.bicycle),
+                title: Text("Road Bike"),
+                subtitle: Text("Rp5.000.000"),
+                trailing: Icon(FontAwesomeIcons.bagShopping),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.monitor),
+                title: Text('Monitor'),
+                subtitle: Text("Rp1.500.000"),
+                trailing: Icon(FontAwesomeIcons.bagShopping),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Card(
+              child: ListTile(
+                leading: Icon(FontAwesomeIcons.keyboard),
+                title: Text("Mechanical Keyboard Wireless"),
+                subtitle: Text("Rp700.000"),
+                trailing: Icon(FontAwesomeIcons.bagShopping),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.mouse),
+                title: Text("Wireless Mouse"),
+                subtitle: Text("Rp500.000"),
+                trailing: Icon(FontAwesomeIcons.bagShopping),
+              ),
+            ),
           ),
         ],
       ),
